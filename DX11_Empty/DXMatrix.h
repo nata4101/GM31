0356f65ca01c678de8679046295bf33347007b12
nata4101 <riki1998911@gmail.com>
2021-06-29 12:56:36 +0900

    作業中

--- DX11_Empty/DXMatrix.h
+++ DX11_Empty/DXMatrix.h
@@ -7,10 +7,57 @@ using namespace DirectX;
 class DXMatrix : public XMFLOAT4X4
 {
 public:
-	DXMatrix(){}
+	DXMatrix(XMMATRIX a){XMStoreFloat4x4(this, a); return; }
+	DXMatrix() { XMStoreFloat4x4(this, XMMatrixIdentity()); return; }
 	~DXMatrix(){}
 
-	DXMatrix operator+ (DXMatrix a){ return }
+	DXMatrix operator+ (DXMatrix a) 
+	{ 
+		DXMatrix buff; 
+		XMStoreFloat4x4(&buff, XMLoadFloat4x4(this) + XMLoadFloat4x4(&a)); 
+		return buff; 
+	}
+	
+	DXMatrix operator- (DXMatrix a)
+	{
+		DXMatrix buff;
+		XMStoreFloat4x4(&buff, XMLoadFloat4x4(this) - XMLoadFloat4x4(&a));
+		return buff;
+	}
+
+	DXMatrix operator* (DXMatrix a)
+	{
+		DXMatrix buff;
+		XMStoreFloat4x4(&buff, XMLoadFloat4x4(this) * XMLoadFloat4x4(&a));
+		return buff;
+	}
+
+	void operator+= (DXMatrix a)
+	{
+		XMStoreFloat4x4(this, XMLoadFloat4x4(this) + XMLoadFloat4x4(&a));
+		return;
+	}
+
+	void operator-= (DXMatrix a)
+	{
+		XMStoreFloat4x4(this, XMLoadFloat4x4(this) - XMLoadFloat4x4(&a));
+		return;
+	}
+
+	void operator*= (DXMatrix a)
+	{
+		XMStoreFloat4x4(this, XMLoadFloat4x4(this) * XMLoadFloat4x4(&a));
+		return;
+	}
+
+	void operator= (XMMATRIX a)
+	{
+		XMStoreFloat4x4(this, a);
+		return;
+	}
+
+	XMMATRIX GetMatrix(void) { return XMLoadFloat4x4(this); }
+
 
 private:
 
